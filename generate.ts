cb262d88157b185709a6dbb5103952abe6111ecc
kixixixixi <kixixixixi@gmail.com>
2023-02-28 01:23:15 +0900

    Add title display

--- generate.ts
+++ generate.ts
@@ -1,5 +1,6 @@
 import fs from "fs"
 import glob from "glob-promise"
+import readline from "readline"
 
 const styles = `
 <style>
@@ -23,12 +24,21 @@ text-decoration: underline;
 </style>
 `
 const act = async () => {
+  
   const files = await glob.promise("slides/**/*")
-  const links = files
+  const links =  (await Promise.all(files
     .filter(f => f.match(/^slides\/(.*?)\.md$/g))
-    .map(f => f.replace(/^slides\//g, "").replace(/\.md$/g, ""))
-    .map(f => `<li><a href="${f}">${f.replace("_", " ")}</a></li>`)
-    .join("\n")
+    .map(async (f) => {
+      const reader = readline.createInterface({ input: fs.createReadStream(f) })
+      const line: string = await new Promise((resolve) => {
+        reader.on('line', (line) => {
+          reader.close()
+          resolve(line)
+        })
+      })
+      const path = f.replace(/^slides\//g, "").replace(/\.md$/g, "")
+      return `<li><a href="${path}">${line.replace(/^\#\s/g, "")}&nbsp;<small>${path}</small></a></li>`
+    }))).join("\n")
   fs.writeFileSync("dist/index.html", `${styles}<h1>Slides</h1><ul>${links}</ul>`)
 
 }
